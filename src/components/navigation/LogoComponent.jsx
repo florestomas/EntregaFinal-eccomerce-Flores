059f719aea944a9b24116d01e25b8916acc65e79
florestomas <floresmancillatomas@gmail.com>
2024-06-24 01:06:01 -0300

    finish

--- src/components/navigation/LogoComponent.jsx
+++ src/components/navigation/LogoComponent.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export default function LogoComponent(){
 
     return(
-        <Link to='/'>
+        <Link to='/' style={{textDecoration: "none"}}>
             <div className="logo">
                 <img height={50} width={42} src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Boca_Juniors_logo18.svg/865px-Boca_Juniors_logo18.svg.png"></img>
                 <h1 className='title'>Boca <br/>Entradas</h1>
